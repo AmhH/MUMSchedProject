1cf5bdb8ff8c8a09b2cf61af80acd35025058030
filiboy03 <filiboy03@gmail.com>
2017-10-22 19:47:14 -0500

    student reg

--- src/main/java/edu/mum/registersubsystem/impl/RegisterSubsystemFacade.java
+++ src/main/java/edu/mum/registersubsystem/impl/RegisterSubsystemFacade.java
@@ -14,7 +14,9 @@ import edu.mum.domain.Student;
 import edu.mum.domain.Transcript;
 import edu.mum.registersubsystem.RegisterSubsystem;
 import edu.mum.service.EntryService;
+import edu.mum.service.SectionsService;
 import edu.mum.service.StudentService;
+import edu.mum.service.UserProfileService;
 
 @Service
 public class RegisterSubsystemFacade implements RegisterSubsystem{
@@ -23,22 +25,37 @@ public class RegisterSubsystemFacade implements RegisterSubsystem{
 	private EntryService entryservice;
 	@Autowired
 	private StudentService studentService;
+	@Autowired
+	private SectionsService sectionservice;
+	@Autowired
+	UserProfileService userProfileService;
 
 	@Override
 	public List<Section> getListSection() {
-		Student student = new Student();//get the student from the proncipal 
-		List<Section> sections = entryservice.getAllSectionsByEntryId(student.getEntry().getId());
+				
+		//Long id = userProfileService.LoggedInUser().getId();
+		Long id=Long.valueOf(1);
+		
+		//Student student = studentService.getStudentById(id);
+		
+		List<Section> sections = entryservice.getAllSectionsByEntryId(id /*student.getEntry().getId()*/);
 		return sections;
 	}
 
 	@Override
 	public String register(Section section) {
 		List<Course> prerequisites = section.getCourse().getPrerequisite();
-		Student student = new Student();//get the student from the proncipal
+		
+		//Long id = userProfileService.LoggedInUser().getId();
+		Long id = Long.valueOf(8);
+		Student student = studentService.getStudentById(id);
+		
 		if(section.getStudents().size()==section.getLimitCapacity()){
 			return "Section is full";
-		} else if(checkTranscript(prerequisites,student.getId()).equalsIgnoreCase(checkregisteredSections(prerequisites,student.getId()))){
+		} else if(checkTranscript(prerequisites,id).equalsIgnoreCase(checkregisteredSections(prerequisites,student.getId()))){
 			section.getStudents().add(student);
+			sectionservice.saveSection(section);
+			studentService.save(student);
 			return "Success";
 		} else if(!checkTranscript(prerequisites,student.getId()).equalsIgnoreCase("Success")){
 			return checkTranscript(prerequisites,student.getId());
@@ -53,6 +70,7 @@ public class RegisterSubsystemFacade implements RegisterSubsystem{
 	
 	private String checkTranscript(List<Course> preReq, long studentId){
 		Set<Transcript> transcript = studentService.getStudentById(studentId).getTranscript();
+		if(transcript.size()>0){
 		List<Transcript> failCourses = transcript.stream()
 									  .filter(t -> preReq.contains(t.getCourse()))
 									  .filter(t-> t.getGrade().compareToIgnoreCase("B")>0)
@@ -60,11 +78,16 @@ public class RegisterSubsystemFacade implements RegisterSubsystem{
 		if(failCourses.isEmpty())
 			return "Success";		
 		return "Unsatsfactory Grade";
+		}
+		return "Success";
 	}
 	
 	private String checkregisteredSections(List<Course> preReq, long studentId){
 		List<Course> registeredCourse = new ArrayList<>();
-		studentService.getStudentById(studentId).getSections().forEach(s->registeredCourse.add(s.getCourse()));;
+		//Long id = userProfileService.LoggedInUser().getId();
+		Long id = Long.valueOf(8);
+		
+		studentService.getStudentById(id).getSections().forEach(s->registeredCourse.add(s.getCourse()));
 		if(registeredCourse.containsAll(preReq))
 			return "Success";
 		return "Not Registered for PreRequisite";
