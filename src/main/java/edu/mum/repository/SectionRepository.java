34af5ace4cc272dc97b32e099a64f40f2d64a07b
woldt <woldtensae.aman@gmail.com>
2017-10-23 11:56:34 -0500

    page template touch and section CRUD usecase

--- src/main/java/edu/mum/repository/SectionRepository.java
+++ src/main/java/edu/mum/repository/SectionRepository.java
@@ -1,10 +1,13 @@
 package edu.mum.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import edu.mum.domain.Section;
+import edu.mum.domain.Student;
 
 public interface SectionRepository extends CrudRepository<Section, Long>{
 
-
+		public List<Student> findStudentById(Long id);
 }
