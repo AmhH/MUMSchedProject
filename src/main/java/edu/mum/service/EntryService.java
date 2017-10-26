5b3805888f581101f399bbf711893887a894432f
woldt <woldtensae.aman@gmail.com>
2017-10-26 00:22:31 -0500

    final commit

--- src/main/java/edu/mum/service/EntryService.java
+++ src/main/java/edu/mum/service/EntryService.java
@@ -1,10 +1,11 @@
 package edu.mum.service;
 
+import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import edu.mum.domain.Entry;
 import edu.mum.domain.Section;
@@ -17,7 +18,22 @@ public class EntryService {
 	private EntryRepository entryRepository;
 
 	public void saveEntry(Entry entry) {
+		/*List<Block> blocks = new ArrayList<Block>();
+		String entryMonth = entry.getEntryMonth();
+		if(entryMonth.equalsIgnoreCase("January")) {
+			//int monthInt = Calendar.JANUARY;
+			for(int i=2;i<8;i++) {
+				String month = getMonth(i);
+				Block block = new Block();
+				block.setBlockMonth(month);
+				blocks.add(block);
+			}
+		}
+		entry.setBlocks(blocks);*/
 		entryRepository.save(entry);
+	}
+	public String getMonth(int month) {
+	    return new DateFormatSymbols().getMonths()[month-1];
 	}
 
 	public Entry getEntry(Long id) {
